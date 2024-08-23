4d0cf8fe4db3b3762b3886ae3985d23d59547d1b
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-22 21:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ Converter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex4
+{
+    public class Converter
+    {
+        public List<int> conversao(List<string> listaS) 
+        {
+            List<int> listaI = new List<int>();
+            listaI = listaS.ConvertAll(int.Parse);
+            return listaI;
+        }
+        public void logic(List<int> listaI, int segun)
+        {
+            int aux = segun;
+            for(int i =0; i<2; i++)
+            {
+                if (listaI[i] + aux > 60) 
+                {
+                    listaI[i] = +aux;
+                    int valores = listaI[i] / 60;
+                    int resto = listaI[i] % 60;
+                    listaI[i + 1] += valores;
+                    aux = listaI[i + 1];
+                    listaI[i] = resto;
+                }
+            }
+            Console.WriteLine("Os segundos foram: {0}", listaI[0]);
+            Console.WriteLine("Os minutos foram: {0}", listaI[1]);
+            Console.WriteLine("As horas foram: {0}", listaI[2]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex4
+{
+
+    /*Escreva um programa em C# que lê um horário inicial (lê uma hora, minuto e 
+    segundo). A seguir o programa deve ler uma quantidade de segundos.
+    Essa quantidade de segundos deve ser adicionada ao horário inicial e o programa
+    deve escrever o horário final (escrever as horas, minutos e segundos finais)*/
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program() 
+        {
+            List<string> listaS = new List<string>();
+            List<int> listaI = new List<int>();
+            Converter Conversao = new Converter();  
+            string[] horarios = {"segundos","minutos","horas"};
+
+            for(int i = 0; i<3; i++)
+            {
+                Console.Write("Digite os(as) {0}: ", horarios[i]);
+                listaS.Add(Console.ReadLine());
+                listaI = Conversao.conversao(listaS);
+                if(i < 2)
+                {
+                    while (listaI[i] > 60 || listaI[i] < 0)
+                    {
+                        Console.Write("Erro os(as) {0} se limitam entre 0 a 60, digite um novo valor valido: ", horarios[i]);
+                        listaS[i]=(Console.ReadLine());
+                        listaI = Conversao.conversao(listaS);
+                    }
+                }
+            }
+            Console.WriteLine("Digite um novo valor de segundos: ");
+            string seg =(Console.ReadLine());
+            int segfinal = Convert.ToInt32(seg);
+
+            Conversao.logic(listaI, segfinal);
+            Console.ReadKey();
+  
+        }
+    
+    
+    }
+}
